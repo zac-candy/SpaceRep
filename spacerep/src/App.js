f3dd34703491fe434c509606b538090ff8012b54
Chandan <chandanms1312@gmail.com>
2021-11-10 13:35:28 +0530

    create form for topic submission

--- spacerep/src/App.js
+++ spacerep/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+
+  const [topics, setTopics] = useState([])
+  const [newTopic, setNewTopic] = useState('a new topic...')
+
+
+  const handleTopicChange = (event)=> {
+    event.preventDefault()
+    setNewTopic(event.target.value)
+  }
+
+  const addTopic = (event)=> {
+    event.preventDefault()
+    const topicOb = {
+      name: newTopic,
+      curDate: new Date().toISOString(),
+      reps: [this.curDate, this.curDate + 1, this.curDate + 7, this.curDate + 30, this.curDate + 90]
+    }
+
+    setTopics(topics.concat(topicOb))
+  }
+
+
+ 
+
+    return (
+    <div>
+      <h1>SpaceRep</h1>
+      <ul>
+      <h2>Enter the Topic</h2>
+      <form onSubmit = {addTopic}>
+        
+        <p><input onChange = {handleTopicChange}/></p>
+        <p><button type = "submit" >Submit</button></p>
+      </form>
+
+
+      </ul>
+      <h2>Topics to revise today</h2>
+      {topics.m()}
+      <ul>
+
+      </ul>
+     
     </div>
-  );
+  )
 }
+
+
 
 export default App;
