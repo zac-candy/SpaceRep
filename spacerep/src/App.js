df74fcd4f160f17452707d2a4d52986da04bdb13
Chandan <chandanms1312@gmail.com>
2021-11-11 11:43:10 +0530

    finish schedule render

--- spacerep/src/App.js
+++ spacerep/src/App.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 const App = () => {
 
   const [topics, setTopics] = useState([])
-  const [newTopic, setNewTopic] = useState('a new topic...')
+  const [newTopic, setNewTopic] = useState('')
 
 
   const handleTopicChange = (event)=> {
@@ -14,24 +14,35 @@ const App = () => {
 
   const addTopic = (event)=> {
     event.preventDefault()
-    const cd = new Date()
     const topicOb = {
       id : 1,
       name: newTopic,
-      curd : cd
+      curd : new Date()
     }
 
     setTopics(topics.concat(topicOb))
+    setNewTopic('')
+  }
+
+  function addDays(date, days) {
+
   }
 
   const GetDates = ({curDate}) => {
-
-    const rep1 = curDate.setDate(curDate.getDate() + 1)
-
-
+    let example = new Date(curDate)
+    const cur = new Date(curDate)
+    const rep1 = new Date(example.setDate(example.getDate() + 1))
+    const rep2 = new Date(example.setDate(example.getDate() + 7))
+    const rep3 = new Date(example.setDate(example.getDate() + 30))
+    const rep4 = new Date(example.setDate(example.getDate() + 90))
+  
     return (
       <ul>
+        <li>{cur.toDateString()}</li>
         <li>{rep1.toDateString()}</li>
+        <li>{rep2.toDateString()}</li>
+        <li>{rep3.toDateString()}</li>
+        <li>{rep4.toDateString()}</li>
       </ul>
     )
   }
@@ -43,9 +54,9 @@ const App = () => {
       <h1>SpaceRep</h1>
       <ul>
       <h2>Enter the Topic</h2>
-      <form onSubmit = {addTopic}>
+      <form  onSubmit = {addTopic}>
         
-        <p><input onChange = {handleTopicChange}/></p>
+        <p><input value  = {newTopic} onChange = {handleTopicChange}/></p>
         <p><button type = "submit" >Submit</button></p>
       </form>
 
